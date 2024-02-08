1c9ddecddbd284a6548c9f2f38ef3e6e7c11e74d
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-02-08 16:06:32 +0100

    Remove useless js file

--- source/structures/client/buttonType.js
+++
@@ -1,7 +0,0 @@
-export default {
-    Primary: 1,
-    Secondary: 2,
-    Success: 3,
-    Danger: 4,
-    Link: 5
-}
--- source/structures/utils/Collection/index.js
+++
@@ -1,5 +0,0 @@
-import { Collection } from "discord.js";
-
-const afk = new Collection();
-
-export default { afk }
